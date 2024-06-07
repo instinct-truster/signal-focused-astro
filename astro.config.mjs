d3f46ee70d2016bd8dcbb14c92ad008474e94ea7
Lance Turbes <instinct_truster@protonmail.com>
2024-06-07 16:25:04 -0700

    clean up navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321",
-
-  integrations: [preact()],
+  integrations: [preact(), db()]
 });
