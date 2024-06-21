33a40003798401d6c24ab2fa5b14ba1e75a94ea8
Lance Turbes <instinct_truster@protonmail.com>
2024-06-21 16:40:41 -0700

    end of day commit

--- src/todo-app/utils/categories.jsx
+++ src/todo-app/utils/categories.jsx
@@ -1,7 +1,7 @@
 export const categories = [
   { name: "faith", icon: "/icons/church.svg" },
-  { name: "family", icon: "/icons/workout.svg" },
-  { name: "fitness", icon: "/icons/airplane.svg" },
+  { name: "family", icon: "/icons/airplane.svg" },
+  { name: "fitness", icon: "/icons/workout.svg" },
   { name: "finance", icon: "/icons/laptop.svg" },
   { name: "fantasy", icon: "/icons/football.svg" },
 ];
